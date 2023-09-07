6d58371300d4663f4d2e883a500b0be3683c9e62
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-07 08:16:33 -0700

    sucess

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Main - A program that print a line with put function
- * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
