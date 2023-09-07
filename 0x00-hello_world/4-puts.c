2d7af059474355a9eb432d759ecdd257363144ad
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-07 06:54:44 -0700

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include <stdio.h>
+/**
+  *Main - A program that print a line with put function
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
