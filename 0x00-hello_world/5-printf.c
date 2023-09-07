e6ec276f290322560643aaf732eea626b809adab
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-07 08:28:08 -0700

    hddsop

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf 
- * Return: 0(Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
