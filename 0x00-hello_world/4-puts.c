4c1f6a0f2a755d6986c053797313e356d1b57774
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-07 07:09:57 -0700

    etru

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  * Main - A program that print a line with put function
-  * Return: 0 (Success)
-  */
+ * Main - A program that print a line with put function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
