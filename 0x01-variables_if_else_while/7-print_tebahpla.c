b3cb87bc72bb8cbc83b10fe010266491f29d0ded
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-08 09:58:57 -0700

    betty

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        char alphabet = 'z';
+
+        while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+	return (0);
+}
