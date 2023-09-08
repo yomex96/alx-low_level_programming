6cb9a2095cf1fdf0642cc507a87dcb129b17715d
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-08 09:47:17 -0700

    betty

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        int  alphabets = '0';
+
+        while (alphabets <= '9')
+        {
+                putchar(alphabets);
+                alphabets++;
+        }
+	putchar('\n');
+        return (0);
+}
