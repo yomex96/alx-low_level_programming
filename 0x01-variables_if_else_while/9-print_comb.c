ba92f8283c02ed3bf7544f7263a07d6ec75f08e2
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-08 13:06:17 -0700

    tola

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	int num;
+	
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
-	i		continue;
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
