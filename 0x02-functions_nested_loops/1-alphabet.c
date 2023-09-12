5d280a2302173921eb4eb41035aeab017c7db594
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-12 04:01:01 -0700

    pose

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the function of the task
+ * Return: void here
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
