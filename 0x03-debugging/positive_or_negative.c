fd47f6537cf67c279fb9f9772ae83ce8453406b7
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-12 20:09:44 -0700

    sdee

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-/**
-* main - Prints a random number decides whether it is positive,negative,orzero.
-*
-* Return: Always 0.
-*/
-int main(void)
+#include "main.h"
+
+void positive_or_negative
 {
 	int i;
 
