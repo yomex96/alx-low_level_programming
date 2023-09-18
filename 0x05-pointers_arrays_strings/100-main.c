0befd7f4e855cc3212bc97e5861758449347364f
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-18 08:38:46 -0700

    swert

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -3,9 +3,9 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int nb;
@@ -22,7 +22,7 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
-	nb = _atoi("++-  -98 Battery Street; San Francisco, CA 94111 - USA             ");
+	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - US");
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
