1930f1349a030d733eb1e911c4a97854f3f41225
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-20 05:37:07 -0700

    add

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char s1[98];
-	char *p;
+	char *ptr;
 	int i;
 
 	for (i = 0; i < 98 - 1; i++)
@@ -18,12 +18,12 @@ int main(void)
 	}
 	s1[i] = '\0';
 	printf("%s\n", s1);
-	p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
 	printf("%s\n", s1);
-	printf("%s\n", p);
-	p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s\n", ptr);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
 	printf("%s", s1);
-	printf("%s", p);
+	printf("%s", ptr);
 	for (i = 0; i < 98; i++)
 	{
 		if (i % 10)
