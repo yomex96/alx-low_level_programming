7ef106fd601345c4686bdce83b7fb860da6955cf
yomex96 <abayomirobertonawole2@gmail.com>
2023-09-20 05:31:29 -0700

    waser

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!\n";
-	char *p;
+	char *ptr;
 
 	printf("%s\n", s1);
 	printf("%s", s2);
-	p = _strncat(s1, s2, 1);
+	ptr = _strncat(s1, s2, 1);
 	printf("%s\n", s1);
 	printf("%s", s2);
-	printf("%s\n", p);
-	p = _strncat(s1, s2, 1024);
+	printf("%s\n", ptr);
+	ptr = _strncat(s1, s2, 1024);
 	printf("%s", s1);
 	printf("%s", s2);
-	printf("%s", p);
+	printf("%s", ptr);
 	return (0);
 }
