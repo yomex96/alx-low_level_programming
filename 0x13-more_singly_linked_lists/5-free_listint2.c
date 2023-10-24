e353213913f9365e91409ea4bd8960843cf40a90
yomex96 <abayomirobertonawole2@gmail.com>
2023-10-24 00:07:41 -0700

    gate

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *musm;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		nusm = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
