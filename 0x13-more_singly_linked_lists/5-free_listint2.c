838259b6d483e47a1cff5183511aea51b22c14c4
yomex96 <abayomirobertonawole2@gmail.com>
2023-10-24 00:06:32 -0700

    swerve

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *musm;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = (*head)->next;
+		nusm = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
