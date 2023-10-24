909c97f8380257ab9932e93f551128b6e4fd2368
yomex96 <abayomirobertonawole2@gmail.com>
2023-10-23 23:58:09 -0700

    try

--- 0x13-more_singly_linked_lists/5-main.c
+++ 0x13-more_singly_linked_lists/5-main.c
@@ -1,28 +1,22 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code at test files.
- *
- * Return: Always 0.
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
  */
-int main(void)
+void free_listint2(listint_t **head)
 {
-	listint_t *head;
+	listint_t *temp;
 
-	head = NULL;
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
-	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
-	print_listint(head);
-	free_listint2(&head);
-	printf("%p\n", (void *)head);
-	return (0);
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
 }
