9f9816260e64be6f869a31023c1427f15d683859
yomex96 <abayomirobertonawole2@gmail.com>
2023-10-23 23:31:30 -0700

    landlord

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,24 +1,27 @@
-#include "lists.h"
-
-/**
- * print_list - print elements of a list
- * @h: constant
- * Return: the node
+/*
+ * File: 0-print_listint.c
+ * Auth: Brennan D Baraban
  */
 
-size_t print_list(const list_t *h)
-{
-	size_t node;
+#include "lists.h"
+#include <stdio.h>
 
-	for (node = 0; h; node++)
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: The number of nodes in the list_t list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+		nodes++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (node);
+
+	return (nodes);
 }
