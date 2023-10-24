b871864b892a58219d9a67c097662d28d9e50591
yomex96 <abayomirobertonawole2@gmail.com>
2023-10-23 23:25:47 -0700

    salary

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,26 +1,25 @@
 /*
  * File: 0-print_listint.c
- * Auth: Abayomi robert onawole
+ * Auth: Onawole abayomi robert
  */
-
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t list in the file.
- * @h: A pointer to the head of the list_t list in the file .
- * Return: The number of nodes in the list_t list.
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		nodes++;
 		printf("%d\n", h->n);
+		num++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (num);
 }
