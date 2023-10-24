fb8d056b1eb64c686e37e2ff95c3c00208bd8c51
yomex96 <abayomirobertonawole2@gmail.com>
2023-10-24 00:20:45 -0700

    seeni

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (i < index - 1)
+	while (m < index - 1)
 	{
 		if (!temp || !(temp->next))
 			return (-1);
