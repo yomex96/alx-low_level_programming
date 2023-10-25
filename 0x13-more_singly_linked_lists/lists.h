96f02ad08430127b9a3b7b63e9f23c9aa25edf44
yomex96 <abayomirobertonawole2@gmail.com>
2023-10-25 02:59:31 -0700

    most

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,8 @@
 /*
  * File: lists.h
  * Auth: Abayomi Robert Onawole
- * Desc: Header file containing prototypes and definitions for all functions and types written in the 0x12-more_singly_linked_lists directory.
+ * Desc: Header file containing prototypes and definitions for all
+ * functions and types written in the 0x12-more_singly_linked_lists directory.
  *
  */
 #include <stdio.h>
