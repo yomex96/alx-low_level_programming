b6704fd8528be4ce37f370c06ba974d6f14652f1
root <abayomirobertonawole2@gmail.com>
2023-12-11 18:19:05 +0000

    whoops

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/yomex96/alx-low_level_programming/master/0x18-dynamic_libraries/libhacks.so
+wget -P .. https://raw.githubusercontent.com/yomex96/alx-low_level_programming/master/0x18-dynamic_libraries/libhacks.so
 export LD_PRELOAD="$PWD/../libhacks.so"
