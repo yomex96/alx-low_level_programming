8a2b2107ec9aab33611e0bd049d6798af13167a9
root <abayomirobertonawole2@gmail.com>
2023-12-11 17:35:54 +0000

    sede

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+
+/**
+ * add - add two integers 
+ * @a: first init 
+ * @b: second init 
+ * Return: addition
+ */
+int add(int a , int b)
+{
+	return (a + b);
+
+}
+
+
+/**
+ * sub - subtraction two integers 
+ * @a: first init 
+ * @b: second init 
+ * Return: subtracted final result
+ */
+int sub(int a , int b)
+{
+        return (a - b);
+
+}
+
+
+/**
+ * mod - modulus of  two integers 
+ * @a: first init 
+ * @b: second init 
+ * Return: modulus of the answer
+ */
+int mod(int a , int b)
+{
+        return (a % b);
+
+}
+
+
+/**
+ * mul - multiply two integers 
+ * @a: first init 
+ * @b: second init 
+ * Return: multiply the  final result
+ */
+int mul(int a , int b)
+{
+        return (a * b);
+
+}
+
+
+/**
+ * div - divide  two integers 
+ * @a: first init 
+ * @b: second init 
+ * Return: divide to get the  final result
+ */
+int my_div(int a , int b)
+{
+        return (a / b);
+
+}
