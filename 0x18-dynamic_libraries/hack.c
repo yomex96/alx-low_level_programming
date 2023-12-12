8538c001be043271a4ea4230d51e183ab03a492c
root <abayomirobertonawole2@gmail.com>
2023-12-12 07:00:09 +0000

    houst

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -4,7 +4,7 @@
 
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 +9\n", 17);
+	write(1, "9 8 10 24 75 -9\n", 17);
 	write(1, "congratulations, you win the jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
